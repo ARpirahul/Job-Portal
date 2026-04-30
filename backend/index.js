3ebae58c5e779bf3edc962d5d1ad16af794e8ad0
ARpirahul <rahulkumarsaxena9988@gmail.com>
2026-04-30 19:03:37 +0530

    error solved

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') }); // ✅ Exact path
+
+
 import express from "express";
 import cookieParser from "cookie-parser"; 
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+// dotenv.config({});
 const app = express();
 
 // middleware
