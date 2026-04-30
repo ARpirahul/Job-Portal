1b5f1640037cb92d4d6280345cbb3ff6e25762df
ARpirahul <rahulkumarsaxena9988@gmail.com>
2026-04-30 17:57:50 +0530

    error solved

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,16 +1,4 @@
-// Ye URL tab update karna jab backend Render pe deploy ho jaye
-export const USER_API_END_POINT = import.meta.env.VITE_API_URL 
-  ? `${import.meta.env.VITE_API_URL}/api/v1/user`
-  : "https://jobwebsite-youtube-1.onrender.com/api/v1/user";
-
-export const JOB_API_END_POINT = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api/v1/job`
-  : "https://jobwebsite-youtube-1.onrender.com/api/v1/job";
-
-export const Application_API_END_POINT = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api/v1/application`
-  : "https://jobwebsite-youtube-1.onrender.com/api/v1/application";
-
-export const Company_API_END_POINT = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api/v1/company`
-  : "https://jobwebsite-youtube-1.onrender.com/api/v1/company";
+export const USER_API_END_POINT = "https://jobportal-backend-977v.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobportal-backend-977v.onrender.com/api/v1/job";
+export const Application_API_END_POINT = "https://jobportal-backend-977v.onrender.com/api/v1/application";
+export const Company_API_END_POINT = "https://jobportal-backend-977v.onrender.com/api/v1/company";
