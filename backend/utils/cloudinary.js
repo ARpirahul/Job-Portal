9f3fb3a1b45c284ac10b9640dbf83e24ed80992a
ARpirahul <rahulkumarsaxena9988@gmail.com>
2026-05-09 11:57:44 +0530

    cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,21 @@
+// import {v2 as cloudinary} from "cloudinary";
+
+// cloudinary.config({
+//     cloud_name: "dd4c83gko",
+//     api_key: "946385332191173",
+//     api_secret: "0-XmeTX7Kn3DMDSLktlIBdD9sSo"
+// });
+
+// export default cloudinary;
+
+
+
 import {v2 as cloudinary} from "cloudinary";
 
 cloudinary.config({
-    cloud_name: "dd4c83gko",
-    api_key: "946385332191173",
-    api_secret: "0-XmeTX7Kn3DMDSLktlIBdD9sSo"
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // ✅ moved to env vars
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
